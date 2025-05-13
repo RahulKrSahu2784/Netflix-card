164526e9844c101446757ac6771c932278996956
Rahul kumar <arar7252784@gmail.com>
2025-05-13 13:39:36 +0530

    add styled component

--- src/SeriesCard.jsx
+++ src/SeriesCard.jsx
@@ -1,41 +1,59 @@
 import React from 'react'
-
+import styled from 'styled-components'
 const SeriesCard = (props) => {
 
-    const {img_url, name, rating, description, cast, genre, watch_url} = props.curElem;
+  const { img_url, name, rating, description, cast, genre, watch_url } = props.curElem;
 
-    const btnStyle = {
-        padding:"1.2rem 2.4rem",
-        border:"none",
-        fontSize:"1.6rem",
-        backgroundColor:`${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
-        color:"var(--btn-color)",
-        fontWeight:"bold",
-        cursor:"pointer"
-    }
+  // ------------styled components using style object----------------
+  // const rahulStyle = styled.button({
+  //   padding:"1.2rem 2.4rem",
+  //   border:"none",
+  //   fontSize:"1.6rem",
+  //   backgroundColor:`${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
+  //   color:"var(--btn-color)",
+  //   fontWeight:"bold",
+  //   cursor:"pointer"
+  // });
 
-    const ratingClass = rating >= 8.5 ? "super_hit" : "average";
+  // ------------styled components using Template literals-------------
+  const rahulStyle = styled.button`
+      padding:1.2rem 2.4rem;
+      border:none;
+      font-size:1.6rem;
+      background-color:${(props) => props.rating >= 8.5 ? "#7dcea0" : "#f7dc6f"};
+      color:var(--btn-color);
+      font-weight:bold;
+      cursor:pointer;
+    `;
+
+  const styleh3 = styled.h3`
+    font-size: 1.6rem;
+    color:red;
+    text-transform: capitalize;
+    `;
+
+  const ratingClass = rating >= 8.5 ? "super_hit" : "average";
   return (
     <li className='card'>
-    <div>
-      <img src={img_url} alt={name} width="40%" height="60%" />
-    </div>
-    <div className='card-content'>
-    <h2>Name: {name}</h2>
-    <h3>Rating: <span className={`rating ${ratingClass}`}> {rating} </span> </h3>
-    <p>Summary: {description}</p>
-    <p>Genre: {genre.join(", ")}</p>
-    <p>Cast: {cast.join(", ")}</p>
-    <a href={watch_url} target="blank">
-      <button style={btnStyle}>Watch Now</button>
-    </a>
-    </div>
-    {/* <button>{age < 18 ? "Not Available" : "Watch Now"}</button> */}
-    {/* <p>{students.length && "No students found"}</p> */}
-    {/* First solution */}
-    {/* <p>{students.length === 0 && "No students found"}</p> */}
-    {/* <p>Number of Students: {students.length}</p> */}
-  </li>
+      <div>
+        <img src={img_url} alt={name} width="40%" height="60%" />
+      </div>
+      <div className='card-content'>
+        <h2>Name: {name}</h2>
+        <styleh3>Rating: <span className={`rating ${ratingClass}`}> {rating} </span></styleh3>
+        <p>Summary: {description}</p>
+        <p>Genre: {genre.join(", ")}</p>
+        <p>Cast: {cast.join(", ")}</p>
+        <a href={watch_url} target="blank">
+          <rahulStyle>Watch Now</rahulStyle>
+        </a>
+      </div>
+      {/* <button>{age < 18 ? "Not Available" : "Watch Now"}</button> */}
+      {/* <p>{students.length && "No students found"}</p> */}
+      {/* First solution */}
+      {/* <p>{students.length === 0 && "No students found"}</p> */}
+      {/* <p>Number of Students: {students.length}</p> */}
+    </li>
   )
 }
 
